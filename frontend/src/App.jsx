02c5013d5b080d3f012f899885055347fc007cd7
Deepak Sharma <deepak.sh798@gmail.com>
2025-11-18 11:19:49 +0530

    fix tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-amber-400">Hello!</div>
+    </>
+  );
+}
+
+export default App;
